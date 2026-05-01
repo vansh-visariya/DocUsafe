1cde012ccd5d30e745df10ca788d48ea0a8669b4
vansh visariya <vanshvisariya1985@gmail.com>
2026-05-01 16:40:39 +0530

    change the complete codebase from next to java

---
+++ src/main/java/com/example/DocUsafe/DocUsafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.DocUsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocUsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocUsafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DocUsafe/DocUsafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DocUsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocUsafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
